4c8e55a113f14fdb835c12cdcbb7703eea97629b
vex.designs <pixverse.official00@gmail.com>
2026-06-21 15:36:47 +0530

    Update quran.js

--- js/quran.js
+++ js/quran.js
@@ -1,1 +1,21 @@
+const surahList = document.getElementById("surahList");
 
+fetch("https://api.alquran.cloud/v1/surah")
+.then(res => res.json())
+.then(data => {
+
+    data.data.forEach(surah => {
+
+        surahList.innerHTML += `
+        <div class="surah-card">
+
+            <h3>
+                ${surah.number}. ${surah.englishName}
+            </h3>
+
+        </div>
+        `;
+
+    });
+
+});
