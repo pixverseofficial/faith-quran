99b85632dba7b17f9c3c5c041075019f86af1962
vex.designs <pixverse.official00@gmail.com>
2026-06-21 17:44:47 +0530

    Update surah.js

--- js/surah.js
+++ js/surah.js
@@ -38,6 +38,22 @@ fetch(`https://api.alquran.cloud/v1/surah/${surahId}/quran-uthmani`)
 
     result.data.ayahs.forEach(ayah => {
 
+        if (
+
+            ayah.numberInSurah === 1 &&
+
+            surahId != 1 &&
+
+            surahId != 9 &&
+
+            ayah.text.includes("بِسْمِ")
+
+        ) {
+
+            return;
+
+        }
+
         html += `
 
             ${ayah.text}
