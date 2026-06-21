dac7ed4df907da0b77744e7447da269d4b5cd6df
vex.designs <pixverse.official00@gmail.com>
2026-06-21 17:19:04 +0530

    Update surah.js

--- js/surah.js
+++ js/surah.js
@@ -1,4 +1,14 @@
 const params = new URLSearchParams(window.location.search);
+
 const surahId = params.get("surah");
+
 console.log(surahId);
 
+fetch(`https://api.alquran.cloud/v1/surah/${surahId}`)
+.then(res => res.json())
+.then(data => {
+
+    document.getElementById("surahName").innerText =
+    data.data.englishName;
+
+});
